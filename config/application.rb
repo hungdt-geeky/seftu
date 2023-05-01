01f80029c471f2c469b6b3fb9831c178e8ccbb32
Dam Tien Hung <hungdt1103@gmail.com>
2023-05-01 16:57:32 +0700

    Update config

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,11 @@ module Seftu
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Asia/Bangkok'
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
+    config.i18n.default_locale = :en
     config.generators.system_tests = nil
   end
 end
